927c533e9e098b3ba9a59363a97a635660580175
JThrisha123 <144303225+JThrisha123@users.noreply.github.com>
2023-10-11 20:29:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-class student:
+def linearSearchProduct(productList,targetProduct):  
+  indicates = []
 
-  def __init__(self, name, roll_number, cgpa):
-    self.name = name
-    self.roll_number = roll_number
-    self.cgpa = cgpa
+  for index,product in enumerate(productList):
+    if product == targetProduct:
+      indicates.append(index)
 
-
-def sort_students(student_list):
-
-  sorted_students = sorted(student_list,key=lambda student: student.cgpa,reverse=True)
+  return indicates
 
 
-  
-  return sorted_students
-  
 
-
-students = [
-    student("Hari", "A123", 7.8),
-    student("sri", "A124",  8.9),
-    student("saumya", "A125", 9.1),
-    student("Mahi", "A126", 9.9),
-]
-
-sorted_students = sort_students(students)
-
-
-for student in sorted_students:
-  print("Name: {}, Roll Number: {},CGPA: {}".format(student.name,
-                                    student.roll_number,
-                    student.cgpa))
+product = ["shoes","boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+result = linearSearchProduct(product,target)
+print(result)
